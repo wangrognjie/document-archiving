080a67de65fefbd64bc60a13ba3a98371f220a8f
wangrongjie <ddwrj2012@163.com>
2024-03-07 23:04:24 +0800

    feat(init): 初始化项目
    创建一个新项目，树立一个小目标。

---
+++ src/main/java/com/jeffry/enhancement/document/archiving/DocumentArchivingApplication.java
@@ -0,0 +1,13 @@
+package com.jeffry.enhancement.document.archiving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentArchivingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentArchivingApplication.class, args);
+	}
+
+}
